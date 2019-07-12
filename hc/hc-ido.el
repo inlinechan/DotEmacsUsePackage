f58101e3e44558fd40c19ff206eea04fd82c7a08
Hyungchan Kim <inlinechan@gmail.com>
2019-07-12 10:32:12 +0900

    Adjust ido extension order

--- hc/hc-ido.el
+++ hc/hc-ido.el
@@ -3,8 +3,8 @@
 (ido-mode 1)
 
 ;; this setting will force Ido to always create a new buffer (in C-x b) if the name does not exist
-(setq ido-create-new-buffer 'always)
+;; (setq ido-create-new-buffer 'always)
 
-(setq ido-file-extensions-order '(".cc" ".cpp" ".c" ".h" ".txt" ".org" ".html"))
+(setq ido-file-extensions-order '(".cc" ".cpp" ".c" ".h" ".bb" ".bbappend" ".bbclass" ".txt" ".org" ".html"))
 
 (provide 'hc-ido)
