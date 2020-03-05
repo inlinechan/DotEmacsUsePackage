237505a408da89628451e90377fa043d1d72ca27
Hyungchan Kim <inlinechan@gmail.com>
2020-03-05 23:24:15 +0900

    Add powerline, ace-window, and braket-hydra

--- lisp/hc-local.el
+++ lisp/hc-local.el
@@ -7,11 +7,37 @@
 ;; Put your experimental things in here
 ;; then move them to init.el when becoming stable
 
-(diminish 'auto-revert-mode "")
-(diminish 'hc-keys "")
-(diminish 'company-mode "Co")
-(diminish 'flycheck-mode "FC")
-(diminish 'eldoc-mode "")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; powerline
+(use-package powerline
+  :config
+  (powerline-default-theme))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ace-window
+(use-package ace-window
+  :config
+  (global-set-key (kbd "M-o") 'ace-window))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://github.com/abo-abo/hydra/wiki/Emacs
+(use-package hydra
+  ;; (global-set-key "[" #'braket-hydra/body)
+  :bind (("[" . braket-hydra/body))
+  :config
+  (defun braket-hydra-pre ()
+    (insert "[")
+    (let ((timer (timer-create)))
+      (timer-set-time timer (timer-relative-time (current-time) 0.3))
+      (timer-set-function timer 'hydra-keyboard-quit)
+      (timer-activate timer)))
+
+  (defhydra braket-hydra (:body-pre braket-hydra-pre
+                                    :color blue
+                                    :hint nil)
+    ;; (zap-to-char -1 ?x)
+    ("s" (progn (undo) (magit-status)))
+    ("f" (progn (undo) (ido-find-file)))))
 
 (provide 'hc-local)
 
