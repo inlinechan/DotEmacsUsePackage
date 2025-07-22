048ee8ecf0f01af19bff9bd8035f7263138c16bf
Hyungchan Kim <hyungchan2.kim@lge.com>
2025-07-23 08:28:51 +0900

    Replace ido-completing-read with completing-read

--- hc/hc-general.el
+++ hc/hc-general.el
@@ -145,10 +145,10 @@ STEP ignored for now."
 ;; 50 files ought to be enough.
 (setq recentf-max-saved-items 50)
 
-(defun ido-recentf-open ()
+(defun hc/recentf-open ()
   "Use `ido-completing-read' to \\[find-file] a recent file."
   (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+  (if (find-file (completing-read "Find recent file: " recentf-list))
       (message "Opening file...")
     (message "Aborting")))
 
